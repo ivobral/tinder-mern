d4b2ce35f9a608adac0274523c60afcef123eb3c
ivobral <ivobral@hotmail.com>
2021-10-16 12:51:57 +0200

    Cards and footer buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+
 import Header from './Header';
+import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons';
 
 function App() {
   return (
     <div className="app">
         <Header />
-        {/* TInderCards*/}
-        {/* SwipeButtons*/}
+        <TinderCards />
+        <SwipeButtons />
     </div>
   );
 }
